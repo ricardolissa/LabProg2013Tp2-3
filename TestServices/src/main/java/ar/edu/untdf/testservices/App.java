2786333e0344fd029064a6a1fd58f2a3e3d7593f
matiasgel <matiasgel@hotmail.com>
2013-04-17 17:51:59 -0300

    agregado test services

---
+++ TestServices/src/main/java/ar/edu/untdf/testservices/App.java
@@ -0,0 +1,13 @@
+package ar.edu.untdf.testservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
